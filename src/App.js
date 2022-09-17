a5f0bb67b1c132e72f6ced43f7921ff771c05a5d
sujon-chambugong <sujonchambugong@gmail.com>
2022-09-18 01:51:22 +0600

    fetching data from json data

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Home from './components/Pages/Home/Home';
 
 function App() {
   return (
-    <div className="App">
+    <div className="max-w-[1400]">
       <Header />
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
--- src/components/Pages/Home/Home.js
+++ src/components/Pages/Home/Home.js
@@ -7,17 +7,17 @@ const Home = () => {
         <div>
             <h1>Home Page</h1>
             <h1>data: {psycho.length}</h1>
-            <div>
-                <div className="card card-compact w-96 bg-base-100 shadow-xl">
-                    <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+            <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3'>
+                { psycho && psycho.map(sycho => <div key={sycho.id} className="card card-compact w-56 bg-base-100 shadow-xl">
+                    <figure><img className='w-100 h-56' src={sycho.img} alt="profile-img" /></figure>
                     <div className="card-body">
                         <h2 className="card-title">Shoes!</h2>
                         <p>If a dog chews shoes whose shoes does he choose?</p>
                         <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Buy Now</button>
+                        <button className="btn btn-primary">Book Now</button>
                         </div>
                     </div>
-                </div>
+                </div>)}
             </div>
         </div>
     );
